cf653afc73e6e1c1b9b7b0e6130c62b48ca90a7b
RodrigoAlencarNEW <123181524+RodrigoAlencarNEW@users.noreply.github.com>
2025-01-07 21:54:17 -0300

    Refactor: melhorando a estrutura do código.
    Implementado carregamento dinâmico de elementos HTML.
    Adicionado cart.js para processar itens ao adicionar ao carrinho.
    Adicionada funcionalidade aos botões para interagir com o carrinho de compras.
    Agora as informações dos produtos são enviadas ao carrinho ao clicar nos botões.
    Melhoria na estrutura do código para maior reutilização.

---
+++ js/amazon.js
@@ -0,0 +1,90 @@
+let productGrid = document.querySelector(".products-grid");
+let cartItems = document.querySelector("#js-cart-quantity");
+
+let productsHTML = "";
+
+products.forEach((product) => {
+  productsHTML += `<div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}">
+        </div>
+
+        <div class="product-name limit-text-to-2-lines ">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars" src="images/ratings/rating-${
+            product.rating.stars * 10
+          }.png">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          $${product.priceCents / 100}
+        </div>
+
+        <div class="product-quantity-container">
+          <select class="">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary" data-product-id="${
+          product.id
+        }">
+          Add to Cart
+        </button>
+      </div>`;
+
+  productGrid.innerHTML = productsHTML;
+});
+
+let addToCartButton = document.querySelectorAll(".add-to-cart-button");
+
+addToCartButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    const selectedItem = button.parentElement.querySelector("select");
+    selectedItem.classList.add(`js-selected-${productId}`);
+
+    let inCart;
+
+    cart.forEach((item) => {
+      if (productId === item.id) {
+        inCart = item;
+      }
+    });
+
+    if (inCart) inCart.quantity += Number(selectedItem.value);
+
+    if (!inCart) {
+      cart.push({
+        id: productId,
+        quantity: Number(selectedItem.value),
+      });
+    }
+
+    cartItems.innerHTML = cart.length;
+    console.log(cart);
+  });
+});
