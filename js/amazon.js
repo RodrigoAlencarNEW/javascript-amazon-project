fa78d7b06ac2c7a0281757aea30d1fbabd4a7c21
RodrigoAlencarNEW <123181524+RodrigoAlencarNEW@users.noreply.github.com>
2025-01-10 20:34:34 -0300

    Refactor: ajuste na funcionalidade da pagina de produtos.
    - Aplicada a função de conversão de moedas já criada para facilitar a leitura do código.

--- js/amazon.js
+++ js/amazon.js
@@ -1,5 +1,6 @@
-import { cart, addCart } from "../data/cart.js";
+import { addCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { convertCentsToDollars } from "./Utils/convertCentsToDollars.js";
 
 let productGrid = document.querySelector(".products-grid");
 
@@ -25,7 +26,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-          $${product.priceCents / 100}
+          $${convertCentsToDollars(product.priceCents)}
         </div>
 
         <div class="product-quantity-container">
